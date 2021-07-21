75cfab47cd82daaa0c065ebdd30fd22813a2de22
Evandio Pereira <evandiopereira@gmail.com>
2021-07-21 19:16:40 -0300

    Primeiro Commit

--- src/main/java/com/springbootcourse/course/CourseApplication.java
+++ src/main/java/com/springbootcourse/course/CourseApplication.java
@@ -2,7 +2,7 @@ package com.springbootcourse.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//teste
 @SpringBootApplication
 public class CourseApplication {
 
