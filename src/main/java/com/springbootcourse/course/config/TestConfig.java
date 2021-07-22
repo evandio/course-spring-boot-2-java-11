a94a58ce3f8eedae09da1d9d6e7b5cea12e1e2e4
Evandio Pereira <evandiopereira@gmail.com>
2021-07-21 22:17:39 -0300

    JPA repository, debendency injecton, database seeding

---
+++ src/main/java/com/springbootcourse/course/config/TestConfig.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.springbootcourse.course.config;
+
+import com.springbootcourse.course.entities.User;
+import com.springbootcourse.course.repositories.UserRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ *
+ * @author evand
+ */
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
