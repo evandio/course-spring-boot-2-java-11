24e9616f1155de707f83a3a136884be09ecadd43
Evandio Pereira <evandiopereira@gmail.com>
2021-07-31 15:33:55 -0300

    User delete

--- src/main/java/com/springbootcourse/course/servicies/UserService.java
+++ src/main/java/com/springbootcourse/course/servicies/UserService.java
@@ -18,20 +18,24 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserService {
-
+    
     @Autowired
     private UserRepository repository;
-
+    
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
-
+    
     public List<User> findAll() {
         return repository.findAll();
     }
-
+    
     public User insert(User obj) {
         return repository.save(obj);
+    }
+    
+    public void delete(Long id) {
+        repository.deleteById(id);
     }
 }
