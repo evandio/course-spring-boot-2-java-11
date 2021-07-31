7c9881f934e0147c0663cd834574df050127ac9b
Evandio Pereira <evandiopereira@gmail.com>
2021-07-31 15:55:33 -0300

    User update

--- src/main/java/com/springbootcourse/course/servicies/UserService.java
+++ src/main/java/com/springbootcourse/course/servicies/UserService.java
@@ -18,24 +18,36 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository repository;
-    
+
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
-    
+
     public List<User> findAll() {
         return repository.findAll();
     }
-    
+
     public User insert(User obj) {
         return repository.save(obj);
     }
-    
+
     public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        Optional<User> entity = repository.findById(id);
+        updateData(entity.get(), obj);
+        return repository.save(entity.get());
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
